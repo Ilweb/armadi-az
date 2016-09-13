906bdda2219980d12fe167efdefa57deeb5b9f67
mysticizm <ivan.lisnik@ilweb.eu>
2016-09-13 16:44:27 +0300

    Много хубаво!

--- header.php
+++ header.php
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
 <head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="home.css">
+	<script type="text/javascript" SRC="js/jquery-1.7.1.min.js"></script>
+	<script type="text/javascript" SRC="js/main.js?ver=1"></script>
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<link href="css/theme/jquery-ui-1.8.18.custom.css" rel="stylesheet" type="text/css"/>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
 </head>
-<body class="home" onload="labelPosition()">
+<body class="<?php echo $bodyClass; ?>" onload="labelPosition()">
 <header>
 <div>
 	<div>
