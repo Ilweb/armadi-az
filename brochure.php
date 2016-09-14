dcd8c83b60b6724ea7054ba036f92fa04bf7d1a4
boriana121 <boriana121@abv.bg>
2016-09-14 17:22:01 +0300

    products

---
+++ brochure.php
@@ -0,0 +1,50 @@
+<div class="brochure">
+	<div class="contains">
+		<div><img src="images/left.png" class="prev" onclick="plusSlides(-1)">
+			<div class="big_brochure">	
+			<div class="inner_content fade js-fade fade-in is-paused">
+				<div>
+				<a href="#">
+					<img src="images/product1.png">
+					<div>
+						<h3>Pici Pasta</h3>
+						<p>About Pici Pasta Pici is a traditional flour-and-water Tuscan pasta of fat spaghetti strands which are rolled by hand to achieve a rustic look. </p>
+					</div>
+				</a>	
+				</div>
+				<div>
+				<a href="#">
+					<img src="images/bottle2.png">
+					<div>
+						<h3>Bee Company</h3>
+						<p>Add it to a breakfast table for a golden touch, give it as a unique and delicious treat. From the Savannah Bee Company of Georgia.</p>
+					</div>
+				</a>	
+				</div>
+			</div>
+		<div class="inner_content fade">
+			<div>
+				<a href="#">
+					<img src="images/product1.png">
+					<div>
+						<h3>Pici Pasta</h3>
+						<p>About Pici Pasta Pici is a traditional flour-and-water Tuscan pasta of fat spaghetti strands which are rolled by hand to achieve a rustic look. </p>
+					</div>
+				</a>	
+			</div>
+			<div>
+				<a href="#">
+					<img src="images/bottle2.png">
+					<div>
+						<h3>Bee Company</h3>
+						<p>Add it to a breakfast table for a golden touch, give it as a unique and delicious treat. From the Savannah Bee Company of Georgia.</p>
+					</div>
+				</a>	
+			</div>
+		</div>
+		</div>
+		<img id="right" src="images/left.png" class="next" onclick="plusSlides(1)">
+		</div>
+	</div>
+	<img id="active" src="images/active.png">
+</div>
--- products.php
+++ products.php
@@ -6,4 +6,61 @@ require 'header.php';
 <?php 
 require 'brochure.php';
 ?>
+<div class="second-part">
+<div class="left_menu">
+	<ul>
+		<li>Categories</li>
+		<li>Drinks</li>
+		<li>Process meats</li>
+		<li>Cans & edible</li>
+		<li>Sweets</li>
+		<li>Super food</li>
+		<li>Bio/organic</li>
+		<li>Traditional</li>
+		<li>Molecular gastronomy</li>
+		<li>Accessoaries</li>
+		<li>Others</li>
+	</ul>
+	<ul>
+		<li>Countries</li>
+		<li>France</li>
+		<li>Switzerland</li>
+		<li>Belgium</li>
+		<li>Italy</li>
+		<li>Spain</li>
+		<li>Hungary</li>
+		<li>Bulgaria</li>
+	</ul>
+	<ul>
+		<li>Brands</li>
+	</ul>
+</div>
+<div class="generals">
+	<div>Recommended products</div>
+	<div class="recommended">
+	<div>
+		<img src="images/left.png" class="prev" onclick="plusSlides(-1)">
+		<div class="items">
+			<img src="images/recommended1.png">
+			<div><span>#355</span>Herbal Tea Forte slimming</div>
+			<div>20$</div>
+			<div>
+				<div>Add to wish list</div>
+				<div>Add to cart</div>
+			</div>
+		</div>
+		<div class="items">
+			<img src="images/recommended2.png">
+			<div>
+				<span>#355</span>Herbal Tea Forte slimming
+			</div>
+			<div>20$</div>
+		</div>
+		<div class="items"><img src="images/recommended3.png"><div><span>#355</span>Herbal Tea Forte slimming</div><div>20$</div></div>
+		<div class="items"><img src="images/recommended4.png"><div><span>#355</span>Herbal Tea Forte slimming</div><div>20$</div></div>
+		<img id="right" src="images/left.png" class="next" onclick="plusSlides(1)">
+	</div>
+	</div>
+</div>
+</div>
 <?php require 'footer.php';?>
