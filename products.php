8fdbcb0ad1c31c167dc513d42e6ecc44b1591cd9
boriana121 <boriana121@abv.bg>
2016-09-15 15:22:12 +0300

    product - num2

--- products.php
+++ products.php
@@ -3,6 +3,7 @@
 $bodyClass = '';
 require 'header.php';
 ?>
+<div class="whole_products">
 <?php 
 require 'brochure.php';
 ?>
@@ -43,10 +44,10 @@ require 'brochure.php';
 		<div class="items">
 			<img src="images/recommended1.png">
 			<div><span>#355</span>Herbal Tea Forte slimming</div>
-			<div>20$</div>
-			<div>
-				<div>Add to wish list</div>
-				<div>Add to cart</div>
+			<div class="price">20$</div>
+			<div class="choice">
+				<button id="first_butt">Add to wish list</button>
+				<button id="second_butt">Add to cart</button>
 			</div>
 		</div>
 		<div class="items">
@@ -54,13 +55,52 @@ require 'brochure.php';
 			<div>
 				<span>#355</span>Herbal Tea Forte slimming
 			</div>
-			<div>20$</div>
+			<div class="price">20$</div>
 		</div>
-		<div class="items"><img src="images/recommended3.png"><div><span>#355</span>Herbal Tea Forte slimming</div><div>20$</div></div>
-		<div class="items"><img src="images/recommended4.png"><div><span>#355</span>Herbal Tea Forte slimming</div><div>20$</div></div>
+		<div class="items"><img src="images/recommended3.png"><div><span>#355</span>Herbal Tea Forte slimming</div><div class="price">20$</div></div>
+		<div class="items"><img src="images/recommended4.png"><div><span>#355</span>Herbal Tea Forte slimming</div><div class="price">20$</div></div>
 		<img id="right" src="images/left.png" class="next" onclick="plusSlides(1)">
 	</div>
 	</div>
+</div>
+<div class="generals for_best">
+	<div>Recommended products</div>
+	<div class="recommended">
+	<div>
+		<img src="images/left.png" class="prev" onclick="plusSlides(-1)">
+		<div class="items">
+			<img src="images/recommended1.png">
+			<div><span>#355</span>Herbal Tea Forte slimming</div>
+			<div class="price">20$</div>
+			<div class="choice">
+				<button id="first_butt">Add to wish list</button>
+				<button id="second_butt">Add to cart</button>
+			</div>
+		</div>
+		<div class="items">
+			<img src="images/recommended2.png">
+			<div>
+				<span>#355</span>Herbal Tea Forte slimming
+			</div>
+			<div class="price">20$</div>
+		</div>
+		<div class="items"><img src="images/recommended3.png"><div><span>#355</span>Herbal Tea Forte slimming</div><div class="price">20$</div></div>
+		<div class="items"><img src="images/recommended4.png"><div><span>#355</span>Herbal Tea Forte slimming</div><div class="price">20$</div></div>
+		<img id="right" src="images/left.png" class="next" onclick="plusSlides(1)">
+	</div>
+	</div>
+</div>
+<div class="generals supply">
+	<div>Recommended products</div>
+	<div>
+	<div style="background:url('images/zaabar.png')"></div>
+	<div style="background:url('images/monte.png')"></div>
+	<div style="background:url('images/planthouse.png')"></div>
+	<div style="background:url('images/zaabar2.png')"></div>
+	<div style="background:url('images/zaabar.png')"></div>
+	<div style="background:url('images/zaabar.png')"></div>
+	</div>
+</div>	
 </div>
 </div>
 <?php require 'footer.php';?>
