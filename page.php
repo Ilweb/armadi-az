bab1edc487d8eee961204980dd864262e9eecea9
mysticizm <ivan.lisnik@ilweb.eu>
2016-11-25 17:30:14 +0200

    back...

--- page.php
+++ page.php
@@ -1,13 +1,6 @@
 <?php
 get_header();
 
-if (wc_get_page_id( 'cart' ) == get_the_ID()) {
-  wc_get_template_part('cart/cart');
-
-}
-
-else
-{
 ?>
 
 
@@ -16,12 +9,6 @@ else
 	<?php
 	get_template_part('left_menu');
 	?>
-	
-		
-		<?php
-		if (has_post_thumbnail())
-		{
-			?>
 		
 		<div class="generals">
 		<?php 
@@ -32,24 +19,22 @@ else
 			<h1><?php the_title(); ?></h1>
 				<div class="containerImg">
 				<?php
-				$img_src = wp_get_attachment_image_url( get_post_thumbnail_id(), 'post-thumbnail' );
-				$img_srcset = wp_get_attachment_image_srcset( get_post_thumbnail_id(), 'post-thumbnail' );
-				$alt = get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true);
+				if (has_post_thumbnail())
+				{
+					$img_src = wp_get_attachment_image_url( get_post_thumbnail_id(), 'post-thumbnail' );
+					$img_srcset = wp_get_attachment_image_srcset( get_post_thumbnail_id(), 'post-thumbnail' );
+					$alt = get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true);
+					?>
+					<img src="<?php echo esc_url( $img_src ); ?>"
+						 srcset="<?php echo esc_attr( $img_srcset ); ?>"
+						 sizes="(max-width: 100%) 100vh, 100%" alt="<?php echo $alt; ?>"/>
+					<?php echo get_post(get_post_thumbnail_id())->post_content; 
+				}
 				?>
-				<img src="<?php echo esc_url( $img_src ); ?>"
-					 srcset="<?php echo esc_attr( $img_srcset ); ?>"
-					 sizes="(max-width: 100%) 100vh, 100%" alt="<?php echo $alt; ?>"/>
-				<?php echo get_post(get_post_thumbnail_id())->post_content; ?>
 			</div>
-		
-			<?php 
-		}
-		
-		
-		?>
 		<div class="the_content">
 			<?php
-				the_content();
+			the_content();
 			
 			
 				
@@ -74,6 +59,6 @@ else
 
 
 <?php
-}
+
 get_footer();
 ?>
