f2249ab085acf50af578c4920e5127d85c112e3b
mysticizm <ivan.lisnik@ilweb.eu>
2016-11-30 18:26:13 +0200

    Brochure!!!

--- brochure.php
+++ brochure.php
@@ -1,152 +1,50 @@
 
 
 <div id="slider">
-  <a  class="control_next">></a>
-  <a  class="control_prev"><</a>
+  <a  class="control_next"><i class="fa fa-angle-right"></i></a>
+  <a  class="control_prev"><i class="fa fa-angle-left"></i></a>
   <ul>
-    <li>
-		<div class="img1">
+    
         <?php
         $args = array(
-            'post_type' => 'product',
-
-
-            'posts_per_page' => 1,
+            'post_type' => 'product'
         
             );
 
         $loop = new WP_Query( $args );
         if ( $loop->have_posts() ) {
-            while ( $loop->have_posts() ) : $loop->the_post();
-                wc_get_template_part(  'content', 'product' );
-
+			$i = 0;
+            while ( $loop->have_posts() ) : $loop->the_post(); $i++;
+			global $product;
+			if ($i % 4 == 1)
+			{
+				echo '<li>';
+			}
+            ?>
+			<div class="img1">
+				<a href="<?php echo get_permalink(); ?>">
+				<?php woocommerce_template_loop_product_thumbnail(); ?>
+				<h4><?php the_title(); ?> - <span><?php woocommerce_template_loop_price(); ?></span></a>
+				<a href="<?php echo $product->add_to_cart_url(); ?>"><i class=" fa-shopping-cart " aria-hidden="true"></i></a></h4>
+				<p><?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ); ?></p>
+			</div>
+			<?php
+			if ($i % 4 == 0)
+			{
+				echo '<div class="content">December 2016</div>';
+				echo '</li>';
+			}
             endwhile;
-        } else {
-            echo __( 'No products found' );
-        }
+			if ($i % 4 != 0)
+			{
+				echo '<div class="content">December 2016</div>';
+				echo '</li>';
+			}
+        } 
         wp_reset_postdata();
 
         ?>
-        <div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--description panel entry-content wc-tab"></div>
-        
-			<!--<h4>Уиски 21год. Чивас Роял Салют | 0.70л- <span>243лв.<i class=" fa-shopping-cart " aria-hidden="true" ></i></span></h4>
-		    <p>Една от “перлите в короната” на Чивас Ригал, бутилирано в ръчно изработена 
-			порцеланова гарафа</p></a>-->
-
-       
-           
-
-     
-
-		</div>
-		<div class="img1">
-        
-		<a href="">	<img src="images/img3.png" alt="">
-    		<h4>Уиски 21год. Чивас Роял Салют | 0.70л- <span>243лв.</span><i class=" fa-shopping-cart " ></i></h4>
-            <p>Комплексен, но хармоничен, с кадифена структура и екзотичен аромат на ванилия и крем брюле с плодови нотки.</p></a>
-        </div>
-		<div class="img1">
-     		<a href=""><img src="images/img1.png" alt="">
-    		<h4>Уиски 21год. Чивас Роял Салют | 0.70л- <span>243лв.</span><i class=" fa-shopping-cart " aria-hidden="true"></i></h4>
-            <p>Една от “перлите в короната” на Чивас Ригал, бутилирано в ръчно изработена 
-            порцеланова гарафа</p></a>
-        </div>
-		<div class="img1">
-			<a href=""><img src="images/img3.png">
-    		<h4>Уиски 21год. Чивас Роял Салют | 0.70л- <span>243лв.</span><i class=" fa-shopping-cart " aria-hidden="true"></i></h4>
-            <p>Една от “перлите в короната” на Чивас Ригал, бутилирано в ръчно изработена 
-            порцеланова гарафа</p></a>
-        </div>
-		<div class="content">ноември-декемри</div>
-	</li>
-    <li>
-    	<div class="img1">
-			<a href=""><img src="images/img1.png" alt=""/>
-			<h4>Уиски 21год. Чивас Роял Салют | 0.70л- <span>243лв.</span><i class=" fa-shopping-cart " aria-hidden="true"></i>
-</h4>
-			<p>Първоначален ярък букет с плодово - пастелна мекота, която се развива до аромат на захаросан плод и зряло сено с препечени нотки.</p>
-			</a>
-        </div>
-   		<div class="img1">
-		<a href="">	<img src="images/img1.png" alt=""/>
-			<h4>Уиски 21год. Чивас Роял Салют | 0.70л- <span>243лв.</span><i class=" fa-shopping-cart " aria-hidden="true"></i></h4>
-            <p>Една от “перлите в короната” на Чивас Ригал, бутилирано в ръчно изработена 
-            порцеланова гарафа</p></a>
-        </div>
-		<div class="img1">
-		<a href="">	<img src="images/img1.png" alt=""/>
-			<h4>Уиски 21год. Чивас Роял Салют | 0.70л- <span>243лв.</span><i class=" fa-shopping-cart " aria-hidden="true"></i></h4>
-            <p>Една от “перлите в короната” на Чивас Ригал, бутилирано в ръчно изработена 
-            порцеланова гарафаlorem</p>
-            </a>
-        </div>
-		<div class="img1">
-			<a href=""><img src="images/img1.png" alt=""/>
-   			<h4>Уиски 21год. Чивас Роял Салют | 0.70л- <span>243лв.</span><i class=" fa-shopping-cart " aria-hidden="true"></i></h4>
-            <p>Една от “перлите в короната” на Чивас Ригал, бутилирано в ръчно изработена 
-            порцеланова гарафа</p></a>
-        </div>
-        <div class="content">ноември-декемри</div>
-        <
-	</li>
-    <li>
-    	<div class="slide3">
-    		<div class="productss">
-				<img class="first" src="images/greygus.png" alt=""/>
-				<img class="secondd" src="images/i1.png" alt="">
-				<img class="third" src="images/i2.png" alt="">
-				<img class="forth" src="images/i5.png" alt="">
-				<img class="fifth" src="images/i1.png" alt="">
-				<img class="sixth" src="images/i2.png" alt="">
-				<img class="seven"  src="images/i6.png" alt="">
-	    		<div class="ribbon">
-	    			<img src="images/pandelka.png" alt="">
-	    		</div>
-	    		
-	    		
-			</div>
-    	
- 
- <div class="aboutText">
-    		<a href=""><h2>Водата на живота - 145лв.
-    		<i class=" fa-shopping-cart" aria-hidden="true"></i></h2>
-			<p class="list">Водка Грей Гус | 0.70л</p>
-			<p class="list">Пастет от диво прасе с трюфел и арманяк  Монте Универсалес | 135г</p>
-			<p class="list">Био маслинена паста Олеа Зара Емпелтре | 100г</p>
-			<p class="list">Био сладко от манго | 260г </p>
-			<p class="list"> Кафе на зърна Куоре Еспресо | 250г </p>
-			<p class="list">Сметанов крем с пармезан и трюфел Гилиано Тартюфи | 80г</p>	</a>
-		</div>
-		</div>
-		 <div class="content">ноември-декемри</div>
-    </li>
-    <li>
-    	<div class="slide4">
-    	<a href=""><img  class="myImage" src="images/img1.png" align=""/></a>
-    	<div class="ab">
-    	<a href=""><h3>Уиски ГленДронак 0.70л <br/><span>1169лв</span><i class=" fa-shopping-cart " aria-hidden="true"></i></h3></a>
-    	<p >Сингъл  малц скоч уиски от източнен Хайланд, Шотландия -  лимитирано 
-издание. Силно и гладко едновременно. С  аромат на средиземноморски  
-плодове в шери, омекотен с нюанси на шоколад, портокал и лешников 
-крем. Интензивен и дълъг послевкус.</p>
-	
-
-<a href=""><h3>Хамон  Серано резерва, 7kг (около) 
-. <br/><span>199лв</span><i class=" fa-shopping-cart " aria-hidden="true"></i></h3>
-    	<p class="txt1" >Сертифициран като “Традиционен 
-деликатес”, с международно признато 
-качество. Сурово-сушен 14 месеца. 
-С ниско съдържание на сол.</p>
-<img  class="myImage2" src="images/img6.png" align=""/></a>
-
-
-    		
-    	</div>
-    	 <div class="content">ноември-декемри</div>
-    </li>
-    <li>SLIDE 6</li>
-    <li>SLIDE 7</li>
-    <li>SLIDE 8</li>
+    <li>SLIDE 1</li>
   </ul>  
 </div>
 
