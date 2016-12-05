cc8bb7ea040898d097a82b339d5a5b756bf400ce
mysticizm <ivan.lisnik@ilweb.eu>
2016-12-05 11:14:11 +0200

    no space after functions.php

--- functions.php
+++ functions.php
@@ -118,20 +118,3 @@ function wooc_save_extra_register_fields( $customer_id ) {
 }
 add_action( 'woocommerce_created_customer', 'wooc_save_extra_register_fields' );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
-
-
