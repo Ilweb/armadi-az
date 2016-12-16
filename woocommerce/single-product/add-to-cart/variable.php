02932f64418b8ab990a25118cd84d934e5216fdc
yoannadp <yoanna_petkova2abv.bg>
2016-12-16 18:35:10 +0200

    variable

--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -31,22 +31,24 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
 		<p class="stock out-of-stock"><?php _e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
 	<?php else : ?>
-		<table class="variations" cellspacing="0">
-			<tbody>
+		<div class="variations" cellspacing="0">
+			
 				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
-					<tr>
-						<td class="label"><label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?></label></td>
-						<td class="value">
+					
+						<p class="label"><label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?></label></p>
+						<p class="value">
+						
 							<?php
 								$selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( urldecode( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ) : $product->get_variation_default_attribute( $attribute_name );
 								wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected ) );
 								echo end( $attribute_keys ) === $attribute_name ? apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . __( 'Clear', 'woocommerce' ) . '</a>' ) : '';
 							?>
-						</td>
-					</tr>
+
+						</p>
+					
 				<?php endforeach;?>
 			</tbody>
-		</table>
+		</div>
 
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -14,11 +14,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 ?>
 <div class="woocommerce-variation-add-to-cart variations_button">
-	<?php if ( ! $product->is_sold_individually() ) : ?>
-		<?php woocommerce_quantity_input( array( 'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 ) ); ?>
-	<?php endif; ?>
-	<button type="submit" class="single_add_to_cart_button button alt "><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->id ); ?>" />
-	<input type="hidden" name="product_id" value="<?php echo absint( $product->id ); ?>" />
-	<input type="hidden" name="variation_id" class="variation_id " value="0" />
+	<div id="myForm">
+			<input type='button' value='-' class='minus' field='quantity' />
+			<input type='text' name='quantity' value="1"  class='qty' <?php echo ( isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantityy'] ) : 1 ) ?>/>
+			<input type='button' value='+' class='plus' field='quantity' />
+   </div>
+	<div class="choice">
+			<button type="submit" class="hvr-shadow-radial" id="second_butt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+			<div class="hvr-shadow-radial wishlist"><?php echo do_shortcode( '[yith_wcwl_add_to_wishlist]' ); ?></div>
+		</div>
+		
+	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
+
 </div>
+<script type="text/javascript">
+	
+		
+ $('.plus').click(function () {
+    $(this).prev().val(+$(this).prev().val() + 1);
+});
+$('.minus').click(function () {
+    if ($(this).next().val() > 0) $(this).next().val(+$(this).next().val() - 1);
+});
+</script>
