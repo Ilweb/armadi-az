feb1d16472b5f760a4f3a39bdfd7e2abc921cc4c
yoannadp <yoanna_petkova2abv.bg>
2016-12-23 13:18:29 +0200

    js

--- header.php
+++ header.php
@@ -7,6 +7,8 @@ global $woocommerce;
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo("charset") ?>">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/lightbox.css?v=1">
+	
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
@@ -143,11 +145,11 @@ $(document).ready(function() {
 });
 </script>-->
 <script>
-$(document).ready(function() {
+jQuery(document).ready(function() {
     // This will fire when document is ready:
-    $(window).resize(function() {
+    jQuery(window).resize(function() {
         // This will fire each time the window is resized:
-        if($(window).width() >= 580) {
+        if(jQuery(window).width() >= 580) {
             // if larger or equal
             jQuery("#first").css("position","relative").css("top","0px").css("width","247px").css("display","inline-block").css("right","0");
         } else {
@@ -159,14 +161,14 @@ $(document).ready(function() {
 </script>
 
 <script>
-$(document).ready(function() {
-        $(".left_menu ul:first-of-type .fa").hide();
+jQuery(document).ready(function() {
+        jQuery(".left_menu ul:first-of-type .fa").hide();
 
-        $(".left_menu ul:first-of-type li").mouseover(function() {  // attaches click handler to links       
+        jQuery(".left_menu ul:first-of-type li").mouseover(function() {  // attaches click handler to links       
             // show clickSave element inside the clicked link
-            var ele = $(".fa", this).show();
+            var ele = jQuery(".fa", this).show();
             // hide all other visible clickSave elements
-            $(".left_menu ul:first-of-type .fa:visible").not(ele).hide(); 
+            jQuery(".left_menu ul:first-of-type .fa:visible").not(ele).hide(); 
         });
     }); 
 </script>
@@ -187,32 +189,32 @@ jQuery("#recommendation").click(function()
 
 </script>-->
 <script>
-$(function() {
-$('.additional_information_tab').click(function() {
-	$(this).css("background","white").css("border-bottom-color","white");
-	$('.description_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
-	$('.reviews_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
-	$('.new_member_box_display').html('The text you are adding');
+(function() {
+jQuery('.additional_information_tab').click(function() {
+	jQuery(this).css("background","white").css("border-bottom-color","white");
+	jQuery('.description_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
+	jQuery('.reviews_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
+	jQuery('.new_member_box_display').html('The text you are adding');
 });
 });
 </script>
 <script>
-$(function() {
-$('.reviews_tab').click(function() {
-	$(this).css("background","white").css("border-bottom-color","white");	
-	$('.description_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
-	$('.additional_information_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
-	$('.new_member_box_display').html('Bore');
+jQuery(function() {
+jQuery('.reviews_tab').click(function() {
+	jQuery(this).css("background","white").css("border-bottom-color","white");	
+	jQuery('.description_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
+	jQuery('.additional_information_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
+	jQuery('.new_member_box_display').html('Bore');
 });
 });
 </script>
 <script>
-$(function() {
-$('.description_tab').css("background", "#fff").css("border-bottom","#fff").click(function() {
-	$(this).css("background","white").css("border-bottom-color","white");
-	$('.additional_information_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
-	$('.reviews_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
-	$('.new_member_box_display').html('Herbal remedy that supports weight loss designed for people over the age of 12, who have a tendency to gain weight and who do not lead a healthy life style.During weight reduction therapy it is recommended that fruit and vegetables be consumed.');
+jQuery(function() {
+jQuery('.description_tab').css("background", "#fff").css("border-bottom","#fff").click(function() {
+	jQuery(this).css("background","white").css("border-bottom-color","white");
+	jQuery('.additional_information_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
+	jQuery('.reviews_tab').css("background","#fff5e6").css("border-bottom-color","#d1c6bf");
+	jQuery('.new_member_box_display').html('Herbal remedy that supports weight loss designed for people over the age of 12, who have a tendency to gain weight and who do not lead a healthy life style.During weight reduction therapy it is recommended that fruit and vegetables be consumed.');
 });
 });
 </script>
