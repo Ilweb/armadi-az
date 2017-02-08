0613c35e42a7323dc196d374106d40b9ed38e88a
mysticizm <ivan.lisnik@ilweb.eu>
2017-02-08 13:54:40 +0200

    single.php and index.php

--- index.php
+++ index.php
@@ -3,7 +3,38 @@ get_header();
 
 get_template_part('brochure');
 ?>
+<div class="centerPage">
+	<div class="second-part">
+		<div class="generals">
+		<?php 
+		while (have_posts())
+		{
+			the_post(); 
+			?>
+			<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+				<div class="containerImg">
+				<?php
+				if (has_post_thumbnail())
+				{
+					$img_src = wp_get_attachment_image_url( get_post_thumbnail_id(), 'post-thumbnail' );
+					$img_srcset = wp_get_attachment_image_srcset( get_post_thumbnail_id(), 'post-thumbnail' );
+					$alt = get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true);
+					?>
+					<img src="<?php echo esc_url( $img_src ); ?>"
+						 srcset="<?php echo esc_attr( $img_srcset ); ?>"
+						 sizes="(max-width: 100%) 100vh, 100%" alt="<?php echo $alt; ?>"/>
+					<?php echo get_post(get_post_thumbnail_id())->post_content; 
+				}
+				?>
+				</div>
+			<?php
+		}
+		?>
+		</div>
 
+	</div>
+	
+</div>
 <?php
 get_footer();
 ?>
