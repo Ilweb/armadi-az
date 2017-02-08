9e501e589d866b818c82016ef4de8a01ba82f455
yoannadp <yoanna_petkova@abv.bg>
2017-02-08 15:43:10 +0200

    again

--- index.php
+++ index.php
@@ -23,16 +23,17 @@ get_header();
 					<img src="<?php echo esc_url( $img_src ); ?>"
 						 srcset="<?php echo esc_attr( $img_srcset ); ?>"
 						 sizes="(max-width: 100%) 100vh, 100%" alt="<?php echo $alt; ?>"/>
-						 <div class="after"><h2><?php the_title(); ?></h2></div>
 					<?php echo get_post(get_post_thumbnail_id())->post_content; 
 				}
 				?>
 				</div>
+				<div class="after"><h2><?php the_title(); ?></h2></div>
+				</a>
+			</div>
 			<?php
 		}
 		?>
-		</a>
-		</div>
+		
 		</div>
 
 	</div>
